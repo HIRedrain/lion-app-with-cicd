d75572eba3c2542d3f524bc5b18009bfa890e6cd
LeeHongBi <97448513+HIRedrain@users.noreply.github.com>
2025-01-02 14:39:22 +0900

    Update Student.java

--- src/main/java/actions/entity/Student.java
+++ src/main/java/actions/entity/Student.java
@@ -1,28 +1,28 @@
-// package actions.entity;
+package actions.entity;
 
 
-// import jakarta.persistence.*;
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-// @NoArgsConstructor
-// @AllArgsConstructor
-// @Getter
-// @ToString
-// @Entity
-// public class Student {
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+@Entity
+public class Student {
 
-//     @Id
-//     @Column(name = "sid")
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)  // id 자동 할당
-//     private long studentId;
+    @Id
+    @Column(name = "sid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // id 자동 할당
+    private long studentId;
 
-//     @Column(name = "sname", nullable = false)
-//     private String studentName;
+    @Column(name = "sname", nullable = false)
+    private String studentName;
 
-//     public void updateSname(Student student) { // 학생 이름 변경
-//         this.studentName = student.getStudentName();
-//     }
-// }
+    public void updateSname(Student student) { // 학생 이름 변경
+        this.studentName = student.getStudentName();
+    }
+}
