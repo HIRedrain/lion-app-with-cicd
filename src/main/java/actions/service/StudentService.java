6d34ea48b6af471ecdff1230babb51eae31689cf
LeeHongBi <97448513+HIRedrain@users.noreply.github.com>
2025-01-02 14:39:38 +0900

    Update StudentService.java

--- src/main/java/actions/service/StudentService.java
+++ src/main/java/actions/service/StudentService.java
@@ -1,51 +1,51 @@
-// package actions.service;
+package actions.service;
 
-// import actions.entity.Student;
-// import actions.repository.StudentRepository;
-// import jakarta.persistence.EntityNotFoundException;
-// import jakarta.transaction.Transactional;
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.stereotype.Service;
+import actions.entity.Student;
+import actions.repository.StudentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-// import java.util.List;
+import java.util.List;
 
-// @RequiredArgsConstructor
-// @Service
-// @Transactional
-// public class StudentService {
-//     private final StudentRepository studentRepository;
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class StudentService {
+    private final StudentRepository studentRepository;
 
-//     public List<Student> getAllStudents() { // DB 에 있는 모든 학생 목록 반환
-//         return studentRepository.findAll();
-//     }
+    public List<Student> getAllStudents() { // DB 에 있는 모든 학생 목록 반환
+        return studentRepository.findAll();
+    }
 
-//     public Student getStudentById(long id) { // id 기준 해당 학생 정보 반환
-//         return studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 학생이 존재하지 않습니다."));
-//     }
+    public Student getStudentById(long id) { // id 기준 해당 학생 정보 반환
+        return studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 학생이 존재하지 않습니다."));
+    }
 
-//     public Student registerStudent(Student student) { // 새로운 학생 저장
-//         return studentRepository.save(student);
-//     }
+    public Student registerStudent(Student student) { // 새로운 학생 저장
+        return studentRepository.save(student);
+    }
 
-//     public Student updateStudent(long id, Student student) { // 학생 정보 변경
-//         Student st = studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 학생이 존재하지 않습니다."));
-//         if (st == null) {
-//             return null;
-//         }
+    public Student updateStudent(long id, Student student) { // 학생 정보 변경
+        Student st = studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 학생이 존재하지 않습니다."));
+        if (st == null) {
+            return null;
+        }
 
-//         st.updateSname(student);
+        st.updateSname(student);
 
-//         return studentRepository.save(st);
-//     }
+        return studentRepository.save(st);
+    }
 
-//     public Student deleteDeptById(long id) { // 해당 id 의 학생 정보 삭제
-//         Student student = studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 학생이 존재하지 않습니다."));
-//         studentRepository.delete(student);
+    public Student deleteDeptById(long id) { // 해당 id 의 학생 정보 삭제
+        Student student = studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 학생이 존재하지 않습니다."));
+        studentRepository.delete(student);
 
-//         return student; // 삭제한 학생 정보 반환
-//     }
-
-
+        return student; // 삭제한 학생 정보 반환
+    }
 
 
-// }
+
+
+}
