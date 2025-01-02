99a4f6e819a649681f1d4e32c743ab2600f3315d
LeeHongBi <97448513+HIRedrain@users.noreply.github.com>
2025-01-02 11:46:26 +0900

    Update AppWithActionsApplication.java
    주석 처리

--- src/main/java/actions/AppWithActionsApplication.java
+++ src/main/java/actions/AppWithActionsApplication.java
@@ -1,13 +1,13 @@
-package actions;
+// package actions;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppWithActionsApplication {
+// @SpringBootApplication
+// public class AppWithActionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppWithActionsApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(AppWithActionsApplication.class, args);
+// 	}
 
-}
+// }
