7cac02aceb471b383db4be51bc1df04e58c55813
LeeHongBi <97448513+HIRedrain@users.noreply.github.com>
2025-01-02 14:24:58 +0900

    Update ActionsController.java

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -8,7 +8,7 @@ public class ActionsController {
 	
 	@GetMapping(value = "/api/actions")
 	public String actionsTest() {
-		return "app-with-actions version:0.0.1 - HELLO, WORLD!";
+		return "app-with-actions version:0.0.1 - 2025-01-02-14-24";
 	}
 	
 }
