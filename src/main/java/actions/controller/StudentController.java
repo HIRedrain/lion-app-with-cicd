ed97364ddfa4d6be8f9ab90f9957dc0f6d56dae2
LeeHongBi <97448513+HIRedrain@users.noreply.github.com>
2025-01-02 11:46:07 +0900

    Update StudentController.java
    주석 처리

--- src/main/java/actions/controller/StudentController.java
+++ src/main/java/actions/controller/StudentController.java
@@ -1,56 +1,56 @@
-package actions.controller;
+// package actions.controller;
 
-import actions.entity.Student;
-import actions.service.StudentService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+// import actions.entity.Student;
+// import actions.service.StudentService;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/students")
-public class StudentController {
+// @RequiredArgsConstructor
+// @RestController
+// @RequestMapping("/api/students")
+// public class StudentController {
 
-    private final StudentService studentService;
+//     private final StudentService studentService;
 
-    @GetMapping("")
-    public List<Student> getStudents() { // /api/students 로 오는 Get 요청 처리 => 모든 학생 정보 반환
-        List<Student> students = studentService.getAllStudents();
+//     @GetMapping("")
+//     public List<Student> getStudents() { // /api/students 로 오는 Get 요청 처리 => 모든 학생 정보 반환
+//         List<Student> students = studentService.getAllStudents();
 
-        return students;
-    }
+//         return students;
+//     }
 
-    @GetMapping("/{id}")
-    public Student getStudent(@PathVariable long id) { // /api/students/{id} 로 오는 Get 요청 처리 => id 에 해당하는 학생 정보 반환
-        Student student = studentService.getStudentById(id);
+//     @GetMapping("/{id}")
+//     public Student getStudent(@PathVariable long id) { // /api/students/{id} 로 오는 Get 요청 처리 => id 에 해당하는 학생 정보 반환
+//         Student student = studentService.getStudentById(id);
 
-        return student;
-    }
+//         return student;
+//     }
 
-    @PostMapping("")
-    public Student registerStudent(@RequestBody Student student) { // /api/students 로 오는 post 요청 처리 => 새로운 학생 정보 저장
-        Student newStudent = studentService.registerStudent(student);
+//     @PostMapping("")
+//     public Student registerStudent(@RequestBody Student student) { // /api/students 로 오는 post 요청 처리 => 새로운 학생 정보 저장
+//         Student newStudent = studentService.registerStudent(student);
 
-        return newStudent;
+//         return newStudent;
 
-    }
+//     }
 
-    @PutMapping("/{id}")
-    public Student updateStudent(@PathVariable long id, @RequestBody Student updateSt) { // /api/students/{id} 로 오는 put 요청 처리 => id 에 해당하는 학생 정보 변경
-        Student st = studentService.updateStudent(id, updateSt);
+//     @PutMapping("/{id}")
+//     public Student updateStudent(@PathVariable long id, @RequestBody Student updateSt) { // /api/students/{id} 로 오는 put 요청 처리 => id 에 해당하는 학생 정보 변경
+//         Student st = studentService.updateStudent(id, updateSt);
 
-        return st;
-    }
+//         return st;
+//     }
 
-    @DeleteMapping("/{id}")
-    public Student deleteStudent(@PathVariable long id) {
-        Student delSt = studentService.deleteDeptById(id);
+//     @DeleteMapping("/{id}")
+//     public Student deleteStudent(@PathVariable long id) {
+//         Student delSt = studentService.deleteDeptById(id);
 
-        return delSt;
-    }
-
-
+//         return delSt;
+//     }
 
 
-}
+
+
+// }
